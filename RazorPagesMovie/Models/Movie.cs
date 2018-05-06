2454798830d392ab9a98c64487b6674fb8510ee5
Chris Song <chrisson@microsoft.com>
2018-05-06 14:55:11 -0700

    Added a new column in DB (Rating)

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -16,5 +16,6 @@ namespace RazorPagesMovie.Models
 		public DateTime ReleaseDate { get; set; }
 		public string Genre { get; set; }
 		public decimal Price { get; set; }
+		public string Rating { get; set; }
 	}
 }
